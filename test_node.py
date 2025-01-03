c707c71f50bb1c168ca8730324e7065769abf246
Artur Kujawa <zigfi7@gmail.com>
2025-01-03 01:43:56 +0000

    added asynchronic scan

--- test_node.py
+++ test_node.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'libs'))
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "libs"))
 import zignode
-#─────────────────────────────────────────────────────────────────────────────────────────┤ Functions Specific ├────────────
+
+# ─────────────────────────────────────────────────────────────────────────────────────────┤ Functions Specific ├────────────
+
 
 def test_function_01(arg):
-  zignode.frame(arg)
-  return arg
+    zignode.frame(arg)
+    return arg
 
 
-#─────────────────────────────────────────────────────────────────────────────────────────┤ Functions Specific ├────────────
-if __name__ == '__main__':
-  zignode.auto(locals())
-
-
+# ─────────────────────────────────────────────────────────────────────────────────────────┤ Functions Specific ├────────────
+if __name__ == "__main__":
+    zignode.auto(locals())
